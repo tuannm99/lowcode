36adfb394abd2d009cf90b54353b4a981f41ab62
tuannm99 <gm.tuannm99@gmail.com>
2023-06-05 21:54:38 +0700

    init go

--- api-manager/main.go
+++ api-manager/main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+// --------------------------------- orm
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func testOrm() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	// Migrate the schema
+	db.AutoMigrate(&Product{})
+
+	// Create
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	// Read
+	var product Product
+	db.First(&product, 1)                 // find product with integer primary key
+	db.First(&product, "code = ?", "D42") // find product with code D42
+
+	// Update - update product's price to 200
+	db.Model(&product).Update("Price", 200)
+	// Update - update multiple fields
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	// Delete - delete product
+	db.Delete(&product, 1)
+}
+
+// -------------------------------------------- api
 // album represents data about a record album.
 type album struct {
 	ID     string  `json:"id"`
